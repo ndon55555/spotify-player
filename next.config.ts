e0bceb6c41c73c1c47642333967eca20d559063d
Don Nguyen <don.nguyen@pathai.com>
2025-04-04 16:43:43 -0400

    Improve local dev with NextJS, Docker ignore file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  // Only use standalone output in production
+  output: process.env.NODE_ENV === 'production' ? 'standalone' : undefined,
 };
 
 export default nextConfig;
